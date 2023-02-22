2d7ad9865f2c87deb4c715ae50ec9b10d979744c
W-Mai <1341398182@qq.com>
2023-02-22 15:26:24 +0800

    😄(init): init!!!
    Signed-off-by: W-Mai <1341398182@qq.com>

---
+++ inc/ObjBase.h
@@ -0,0 +1,30 @@
+//
+// Created by W-Mai on 2023/2/21.
+//
+
+#ifndef XWRAPUP_OBJBASE_H
+#define XWRAPUP_OBJBASE_H
+
+class ObjBase {
+private:
+public:
+    ObjBase() {
+    }
+
+    void init() {
+    }
+
+    void gstAttr() {
+    }
+
+    void setAttr() {
+    }
+
+    void execCb() {
+    }
+
+    void registerCb() {
+    }
+};
+
+#endif//XWRAPUP_OBJBASE_H
