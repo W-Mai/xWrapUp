709b2964fc40ab87aae7f742fce211196911a9c3
W-Mai <1341398182@qq.com>
2023-03-07 15:21:00 +0800

    😌(example): update usage
    Signed-off-by: W-Mai <1341398182@qq.com>

--- examples/main.cpp
+++ examples/main.cpp
@@ -5,21 +5,28 @@
 #include <iostream>
 #include <xWrapUp.h>
 
+template<class T, class... ARGS>
+T exec(ObjBase *obj, IDType id, ARGS... args) {
+    T rtn{};
+    obj->exec(id, &rtn, args...);
+    return rtn;
+}
+
 int main() {
     ObjBase::init();
     Button::init();
 
     ObjBase *objBase;
-    auto button = new Button();
-    objBase     = button;
+    auto button  = new Button();
+    objBase      = button;
 
-    bool val_bool;
+    bool visible = false;
     objBase->exec(FE(ObjBase) show, nullptr);
-    objBase->exec(FE(ObjBase) visible, &val_bool);
-    std::cout << val_bool << std::endl;
-    objBase->exec(FE(ObjBase) hide, nullptr);
-    objBase->exec(FE(ObjBase) visible, &val_bool);
-    std::cout << val_bool << std::endl;
+    visible = exec<bool>(objBase, FE(ObjBase) visible);
+    std::cout << "visible: " << visible << std::endl;
+    exec<void *>(objBase, FE(ObjBase) hide);
+    visible = exec<bool>(objBase, FE(ObjBase) visible);
+    std::cout << "visible: " << visible << std::endl;
 
     objBase->setAttr(AE(Button) Test, nullptr, 20);
     int val;
